6e1817c22623022669d91a7e2c53f270cbd699bf
Marty Wallace <marty@martywallace.com>
2016-12-20 20:04:15 +1100

    Move camera.

--- src/game.js
+++ src/game.js
@@ -6,8 +6,22 @@ const game = {
 	world: null,
 
 	init() {
-		this.world = new World();
+		this.world = new World(40, 40);
 		stage.addChild(this.world.graphics);
+		
+		this.world.load({
+			beings: [
+				{ type: 'Wall', x: 5, y: 5 },
+				{ type: 'Wall', x: 5, y: 6 },
+				{ type: 'Wall', x: 5, y: 7 },
+				{ type: 'Hero', x: 4, y: 8 }
+			]
+		});
+
+		view.addEventListener('click', event => {
+			let bounds = view.getBoundingClientRect();
+			this.world.handleClick(event.pageX - bounds.left, event.pageY - bounds.top);
+		});
 
 		this.update();
 	},
--- src/world.js
+++ src/world.js
@@ -1,19 +1,76 @@
+class Being {
+	constructor(world, cell) {
+		this.world = world;
+		this.cell = cell;
+		this.graphics = null;
+	}
+
+	setCell(cell) {
+		if (this.graphics) {
+			this.graphics.x = cell.x * this.world.scale;
+			this.graphics.y = cell.y * this.world.scale;
+		}
+
+		this.cell = cell;
+	}
+
+	update() {
+		//
+	}
+}
+
+class Wall extends Being {
+	constructor(world, cell) {
+		super(world, cell);
+
+		this.graphics = new PIXI.Graphics();
+		this.graphics.beginFill(0x333333);
+		this.graphics.drawRect(0, 0, world.scale, world.scale);
+		
+		this.setCell(cell);
+	}
+}
+
+class Mobile extends Being {
+	//
+}
+
+class Hero extends Mobile {
+	constructor(world, cell) {
+		super(world, cell);
+
+		this.graphics = new PIXI.Graphics();
+		this.graphics.beginFill(0xFF0000);
+		this.graphics.drawRect(0, 0, world.scale, world.scale);
+		this.graphics.endFill();
+
+		this.setCell(cell);
+	}
+}
+
+const beings = {
+	Wall, Hero
+};
+
 class Grid {
 	constructor(width, height) {
 		this.width = width;
 		this.height = height;
 		this.cells = [];
 
-		for (let x = 0; x < width; x++) {
-			this.cells[x] = [];
+		for (let y = 0; y < height; y++) {
+			this.cells[y] = [];
 
-			for (let y = 0; y < height; y++) {
-				this.cells[x][y] = new Cell(this, x, y);
+			for (let x = 0; x < height; x++) {
+				this.cells[y][x] = new Cell(this, x, y);
 			}
 		}
 	}
 
-	find(x, y) {
+	find(x, y, scale = 1) {
+		x = Math.floor(x / scale);
+		y = Math.floor(y / scale);
+
 		if (x < 0 || y < 0 || x >= this.width || y >= this.height) {
 			throw new Error('Trying to get an out of bounds cell.');
 		}
@@ -30,28 +87,91 @@ class Cell {
 	}
 }
 
-class World {
-	constructor() {
+class World extends EventEmitter {
+	constructor(width, height, scale = 40) {
+		super();
+
+		this.scale = scale;
 		this.beings = new List();
-		this.grid = new Grid(100, 100);
 		this.graphics = new PIXI.Container();
+		this.viewing = null;
+
+		this.setupGrid(width, height, scale);
 	}
 
-	drawGrid(scale) {
-		//
+	setupGrid(width, height, scale) {
+		this.grid = new Grid(width, height);
+
+		for (let x = 0; x <= width; x++) {
+			let graphics = new PIXI.Graphics();
+			graphics.lineStyle(3, 0xCCCCCC);
+			graphics.moveTo(0, 0);
+			graphics.lineTo(0, height * scale);
+			graphics.x = x * scale;
+
+			this.graphics.addChild(graphics);
+		}
+
+		for (let y = 0; y <= height; y++) {
+			let graphics = new PIXI.Graphics();
+			graphics.lineStyle(3, 0xCCCCCC);
+			graphics.moveTo(0, 0);
+			graphics.lineTo(width * scale, 0);
+			graphics.y = y * scale;
+
+			this.graphics.addChild(graphics);
+		}
+	}
+
+	handleClick(x, y) {
+		let cell = this.grid.find(x - this.graphics.x, y - this.graphics.y, this.scale);
+
+		this.view(cell);
+	}
+
+	create(type, cell) {
+		let being = new type(this, cell);
+
+		if (being.graphics) {
+			this.graphics.addChild(being.graphics);
+		}
+
+		this.beings.add(being);
+
+		return being;
+	}
+
+	destroy(being) {
+		if (being.graphics) {
+			being.graphics.parent === this.graphics && this.graphics.removeChild(being.graphics);
+		}
+
+		this.beings.remove(being);
+	}
+
+	load(level) {
+		if (typeof level === 'string') {
+			level = JSON.parse(level);
+		}
+
+		level.beings.forEach(def => {
+			this.create(beings[def.type], this.grid.find(def.x, def.y));
+		});
+	}
+
+	unload() {
+		this.beings.forEach(being => this.destroy(being));
+		this.beings = [];
 	}
 
 	update() {
 		this.beings.items.forEach(being => being.update());
 	}
-}
 
-class Being {
-	update() {
-		//
+	view(cell) {
+		this.viewing = cell;
+
+		this.graphics.x = -cell.x * this.scale + (renderer.width / 2) - (this.scale / 2);
+		this.graphics.y = -cell.y * this.scale + (renderer.height / 2) - (this.scale / 2);
 	}
-}
-
-class Mobile extends Being {
-	//
 }
